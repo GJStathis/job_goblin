081400138a88d6c1812f0d8c248d09d13c0b2b7a
Simon <coelhosimon@hotmail.com>
2025-08-27 21:34:28 -0600

    remove voice stuff

--- intake_job_profile_service/src/voice_pipeline.py
+++
@@ -1,5 +0,0 @@
-from agents.voice import SingleAgentVoiceWorkflow, VoicePipeline
-from src.voice_agent import IntakeJobProfileAgent
-
-
-pipeline = VoicePipeline(workflow=SingleAgentVoiceWorkflow(IntakeJobProfileAgent().agent))
